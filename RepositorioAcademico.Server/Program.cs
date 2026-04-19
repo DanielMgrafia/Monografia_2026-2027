0117c832e496683568d03e41b050b675a59e12e2
DanielMonografia <danielmgrafia@gmail.com>
2026-04-19 12:15:26 -0600

    mi primer commit xd

---
+++ RepositorioAcademico.Server/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Servicios
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Archivos estáticos del frontend
+app.UseDefaultFiles();
+app.MapStaticAssets();
+
+// Pipeline
+/*if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}*/
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
